9fdda6b16f32fb1eeba39af78fee6e9fd6034f2a
Louis <louis20019028@gmail.com>
2024-12-18 14:44:21 +0800

    Initial Commit

---
+++ Music/MusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicApp.swift
+//  Music
+//
+//  Created by Louis Chang on 2024/12/18.
+//
+
+import SwiftUI
+
+@main
+struct MusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MusicTests/MusicTests.swift
@@ -0,0 +1,17 @@
+//
+//  MusicTests.swift
+//  MusicTests
+//
+//  Created by Louis Chang on 2024/12/18.
+//
+
+import Testing
+@testable import Music
+
+struct MusicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
